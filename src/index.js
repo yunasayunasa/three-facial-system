4576cee655ecbf2ea9d476b2e47b9fa8031a66c0
yunasayunasa <198059552+yunasayunasa@users.noreply.github.com>
2026-08-29 00:30:50 +0900

    Add public runtime exports

---
+++ src/index.js
@@ -0,0 +1,6 @@
+export { TextureRepository } from './runtime/TextureRepository.js';
+export { TextureAtlasCoverageController } from './runtime/TextureAtlasCoverageController.js';
+export { FaceOverlayController } from './runtime/FaceOverlayController.js';
+export { ExpressionController } from './runtime/ExpressionController.js';
+export { createFacialSystem } from './runtime/createFacialSystem.js';
+export { setupFace } from './runtime/setupFace.js';
